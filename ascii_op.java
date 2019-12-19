60a262ea9ce28bfa4dafb54b33061c6ce4a5d2b8
BaneleMk <banelen7x@gmail.com>
2019-12-19 12:26:56 +0200

    fix messages can now be verified

--- ascii_op.java
+++ ascii_op.java
@@ -28,22 +28,29 @@ public class ascii_op {
         return tempsum;
     }
 
-    public void makefix(String message_elements){
+    // the message_elements are as follows "ID;iNSTRUMENT;QUANTITY;MARKET;PRICE"
+    // example of message_elements "0001;guitar;4;rando;10000"
+    // example output "ID=0001|Instr=guitar|Quant=4|Market=rando|Price=10000|10=185"
+    public String makefix(String message_elements){
         String elements [] = message_elements.split(";");
-        String fixmessage = "ID="+elements[0]+
+        String fixmessage = 
+        "ID="+elements[0]+
         "|Instr="+elements[1]+
         "|Quant="+elements[2]+
         "|Market="+elements[3]+
         "|Price="+elements[4]+"|";
         fixmessage += "10="+ascii_checksum(fixmessage);
-        System.out.println(fixmessage);
+        return fixmessage;
     }
 
+    // This is just taking the full fixmessage recieved
+    // it checking if the checksum matches up or not to the data contained
     public boolean checksum_verification(String message){
-        /*
-            i take the string store it...maybe not i find the checksum 1st things 1st
-        */
-        return true;
+        String elements [] = message.split("10=");
+        if (ascii_checksum(elements[0]) != Integer.valueOf(elements[1]))
+            return false;
+        else 
+            return true;
     }
 
     private long ascii_checksum(String data){
