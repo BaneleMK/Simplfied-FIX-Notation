bf3c95de2e7b46b1b19cadfd3c37d7c52e59a448
BaneleMk <banelen7x@gmail.com>
2019-12-19 12:08:50 +0200

    added the method that allows for simple fix messages to be made

--- ascii_op.java
+++ ascii_op.java
@@ -5,25 +5,38 @@ package ascii.ascii_op;
  */
 public class ascii_op {
     private String _message;
-    private long sum = 0;
-    private long checksum = 0;
+    private long _sum = 0;
+    private long _checksum = 0;
 
     public ascii_op(String message){
         _message = message;
 
-        ascii_sum();
-        ascii_checksum();
+        _sum = ascii_sum(_message);
+        _checksum = ascii_checksum(_message);
     }
 
-    private void ascii_sum(){
-        int length = _message.length();
+    private long ascii_sum(String data){
+        long tempsum = 0;
+        int length = data.length();
         for (int i = 0; i < length ; i++) {
             // the '[space]' and '|' represent the start of header ascii which has a value of 1
-            if (_message.charAt(i) == ' ' || _message.charAt(i) == '|')
-                sum += 1;
+            if (data.charAt(i) == ' ' || data.charAt(i) == '|')
+                tempsum += 1;
             else
-                sum += _message.charAt(i);
+                tempsum += data.charAt(i);
         }
+        return tempsum;
+    }
+
+    public void makefix(String message_elements){
+        String elements [] = message_elements.split(";");
+        String fixmessage = "ID="+elements[0]+
+        "|Instr="+elements[1]+
+        "|Quant="+elements[2]+
+        "|Market="+elements[3]+
+        "|Price="+elements[4]+"|";
+        fixmessage += "10="+ascii_checksum(fixmessage);
+        System.out.println(fixmessage);
     }
 
     public boolean checksum_verification(String message){
@@ -33,15 +46,18 @@ public class ascii_op {
         return true;
     }
 
-    private void ascii_checksum(){
-        checksum = sum % 256;
+    private long ascii_checksum(String data){
+        long temp_sum = ascii_sum(data);
+        long temp_checksum = 0;
+        temp_checksum = temp_sum % 256;
+        return temp_checksum;
     }
 
     public long get_sum(){
-        return sum;
+        return _sum;
     }
 
     public long get_checksum(){
-        return checksum;
+        return _checksum;
     }
 }
