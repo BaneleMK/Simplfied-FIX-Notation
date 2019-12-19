153cbde8638ef21401e8be0412ba7474e8f94075
BaneleMk <banelen7x@gmail.com>
2019-12-19 12:38:42 +0200

    cleaned up the presentation

--- fix_message.java
+++ fix_message.java
@@ -1,23 +1,13 @@
-package ascii.ascii_op;
+package ascii.fix_message;
 
 /**
  * ascii_op
  */
-public class ascii_op {
-    private String _message;
-    private long _sum = 0;
-    private long _checksum = 0;
+public class fix_message {
 
-    public ascii_op(String message){
-        _message = message;
-
-        _sum = ascii_sum(_message);
-        _checksum = ascii_checksum(_message);
-    }
-
-    private long ascii_sum(String data){
+    private long ascii_sum(final String data){
         long tempsum = 0;
-        int length = data.length();
+        final int length = data.length();
         for (int i = 0; i < length ; i++) {
             // the '[space]' and '|' represent the start of header ascii which has a value of 1
             if (data.charAt(i) == ' ' || data.charAt(i) == '|')
@@ -31,8 +21,8 @@ public class ascii_op {
     // the message_elements are as follows "ID;iNSTRUMENT;QUANTITY;MARKET;PRICE"
     // example of message_elements "0001;guitar;4;rando;10000"
     // example output "ID=0001|Instr=guitar|Quant=4|Market=rando|Price=10000|10=185"
-    public String makefix(String message_elements){
-        String elements [] = message_elements.split(";");
+    public String makefix(final String message_elements){
+        final String elements [] = message_elements.split(";");
         String fixmessage = 
         "ID="+elements[0]+
         "|Instr="+elements[1]+
@@ -45,26 +35,20 @@ public class ascii_op {
 
     // This is just taking the full fixmessage recieved
     // it checking if the checksum matches up or not to the data contained
-    public boolean checksum_verification(String message){
-        String elements [] = message.split("10=");
+    public boolean checksum_verification(final String message){
+        final String elements [] = message.split("10=");
         if (ascii_checksum(elements[0]) != Integer.valueOf(elements[1]))
             return false;
         else 
             return true;
     }
 
-    private long ascii_checksum(String data){
-        long temp_sum = ascii_sum(data);
+    // this takes the data without the checksum and counts the ascii to come up with the checksum number
+    private long ascii_checksum(final String data){
+        final long temp_sum = ascii_sum(data);
         long temp_checksum = 0;
         temp_checksum = temp_sum % 256;
         return temp_checksum;
     }
 
-    public long get_sum(){
-        return _sum;
-    }
-
-    public long get_checksum(){
-        return _checksum;
-    }
 }
